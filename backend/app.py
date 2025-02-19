3a6aea7b5c150b15b9c351ba5ab517d1de726dab
alexmilson <vg9176576@gmail.com>
2025-02-19 10:55:55 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
+from model import get_answer
+from symptom_checker import check_symptoms
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Healthcare Chatbot API is running"}
+
+@app.get("/ask")
+def ask_question(question: str):
+    answer = get_answer(question)
+    return {"answer": answer}
+
+@app.get("/symptom_checker")
+def symptom_checker(symptoms: str):
+    symptoms_list = symptoms.split(", ")
+    diseases = check_symptoms(symptoms_list)
+    return {"possible_diseases": diseases}
 
