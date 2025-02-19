2d7d883412b6531368711e3efb78c40a68021f76
alexmilson <vg9176576@gmail.com>
2025-02-19 11:12:45 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,16 @@
+import React from "react";
+import Chatbot from "./components/Chatbot";
+import SymptomChecker from "./components/SymptomChecker";
+
+function App() {
+  return (
+    <div>
+      <h1>Healthcare Chatbot</h1>
+      <Chatbot />
+      <SymptomChecker />
+    </div>
+  );
+}
+
+export default App;
 
